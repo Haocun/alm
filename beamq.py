702ef3af9054f2df397ea9975d8fd69a8d036946
Haocun <haocunyu@gmail.com>
2015-02-11 09:02:50 +0000

    Modify 0211

--- beamq.py
+++ beamq.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class beamq:
+class beamq(object):
     
     """
     -- beamq --
@@ -39,9 +39,9 @@ class beamq:
                 this is also the imaginary part of the q parameter.
     """
     
-    def __init__ (self, q, wavelength = 1064e-9):
+    def __init__ (self, qvalue, wavelength = 1064e-9):  #???
         
-        self.q = q
+        self.q = qvalue
         self.wavelength = wavelength
         self.waistSize = 0.
         self.waistZ = 0.
@@ -51,88 +51,61 @@ class beamq:
         self.rayleighRange = 0.
     
 
-
-    def beamWaistAandZ (self, w0, Z, wavelength = 1064e-9):
+    @staticmethod
+    def beamWaistAandZ(w0, Z, wavelength = 1064e-9):
         
-        self.w0 = w0
-        self.Z = Z
-        self.wavelength  = wavelength
+        ZR = np.pi*self.w0**2/self.wavelength
+        q = self.Z+1j*self.ZR
         
-        self.ZR = np.pi*self.w0**2/self.wavelength
-        self.q = self.Z+1j*self.ZR
+        return beamq(q, wavelength)
         
-        return self
+    
+    @staticmethod
+    def beamWaistAandR(w0, R, wavelength = 1064e-9):
         
+        ZR = np.pi*self.w0**2/self.wavelength
+        q = (1/self.R-1j/self.ZR)**(-1)
+        
+        return beamq(q, wavelength)
     
 
-    def beamWaistAandR (self, w0, R, wavelength = 1064e-9):
+    @staticmethod
+    def beamWidthAandR(w, R, wavelength = 1064e-9):
         
-        self.w0 = w0
-        self.R = R
-        self.wavelength = wavelength
+        Z = self.R/(1+(self.R*self.wavelength/np.pi/self.w**2)**2)
+        ZR = np.sqrt(self.Z*(self.R-self.Z))
+        q = self.Z+1j*self.ZR
         
-        self.ZR = np.pi*self.w0**2/self.wavelength
-        self.q = (1/self.R-1j/self.ZR)**(-1)
+        return beamq(q, wavelength)
         
-        return self
     
+    @staticmethod
+    def transformValue(qvalin, M=np.matrix ('1,0;0,1')):
+        
+        qvalout = (M.item(0,0)*self.qvalin+M.item(0,1))/(M.item(1,0)*self.qvalin+M.item(1,1))
+    
+        return qvalout
+        
+        
+    # data access methods
 
-        
-    def beamWidthAandR (self, w, R, wavelength = 1064e-9):
-        
-        self.w = w
-        self.R = R
-        self.wavelength = wavelength
-        
-        self.Z = self.R/(1+(self.R*self.wavelength/np.pi/self.w**2)**2)
-        self.ZR = np.sqrt(self.Z*(self.R-self.Z))
-        self.q = self.Z+1j*self.ZR
-        
-        return self
-        
-    
-
-    def transformValue (self, qvalin, M=np.matrix ('1,0;0,1')):
-        
-        self.qvalin = qvalin
-        self.qvalout = (M.item(0,0)*self.qvalin+M.item(0,1))/(M.item(1,0)*self.qvalin+M.item(1,1))
-    
-        return self.qvalout
-        
-    
-
-    # constructor and data access methods
-    
-    def beamq (self, qvalue = 0, wavelength = 0): 
-        
-        if qvalue != 0:
-            self.q = qvalue
-            if wavelength != 0:
-                self.wavelength = wavelength    
-        return self 
-        
-    
     def set_q (self, qvalue):
         
         if qvalue.imag < 0:
             raise Exception ("imaginary part of q parameter must be positive")
         
-        else:
-            self.qvalue = qvalue
-            self.q = self.qvalue
-            return self
+        self.q = qvalue
+        return self
             
     
 
     def  set_wavelength (self, newwavelength):
         
-        if newwavelength.imag <= 0:
+        if newwavelength <= 0:
             raise Exception ("wavelength must be positive")
-        
-        else:
-            self.newwavelength = newwavelength
-            self.q = self.newwavelength
-            return self
+
+        self.wavelength = newwavelength
+        return self
     
 
 
@@ -149,36 +122,36 @@ class beamq:
 
 
 
-    # methods for properties
+    # methods for dependent properties
     def get_waistSize (self, qin):   # qin is an object
 
         self.wavelength = qin.wavelength
         self.waistSize = np.sqrt((qin.q.imag)*self.wavelength/np.pi)
 
-        return self.waistSize
+        return self
 
 
 
     def get_rayleighRange (self, qin): # not sure
-        # self.w0 = qin.waistSize
-        self.w0 = qin.get_waistSize(qin)
+        # w0 = qin.waistSize
+        w0 = qin.get_waistSize(qin)
         self.wavelength = qin.wavelength
 
-        self.rayleighRange = np.pi*self.w0**2/self.wavelength
+        self.rayleighRange = np.pi*w0**2/self.wavelength
 
-        return self.rayleighRange
+        return self
 
     # ???why dont use imag part of q directly??? if so
 
 
 
     def get_divergenceAngle (self, qin):
-        self.w0 = qin.get_waistSize(qin)
-        self.zR = qin.get_rayleighRange(qin)
+        w0 = qin.get_waistSize(qin)
+        zR = qin.get_rayleighRange(qin)
 
-        self.divergenceAngle = self.w0/self.zR
+        divergenceAngle = self.w0/self.zR
 
-        return self.divergenceAngle
+        return self
 
 
 
@@ -186,34 +159,34 @@ class beamq:
 
         self.waistZ = qin.q.real
 
-        return self.waistZ
+        return self
 
 
 
     def get_beamWidth (self, qin):
 
-        self.z = qin.get_waistZ(qin)
-        self.zR = qin.get_rayleighRange(qin)
-        self.w0 = qin.get_waistSize(qin)
+        z = qin.get_waistZ(qin)
+        zR = qin.get_rayleighRange(qin)
+        w0 = qin.get_waistSize(qin)
 
-        self.beamWidth = self.w0*np.sqrt(1+(self.z/self.zR)**2)
+        self.beamWidth = w0*np.sqrt(1+(z/zR)**2)
 
-        return self.beamWidth
+        return self
 
 
 
     def get_radiusOfCurvature (self, qin):
 
-        self.z = qin.get_waistZ(qin)
-        self.zR = qin.get_rayleighRange(qin)
+        z = qin.get_waistZ(qin)
+        zR = qin.get_rayleighRange(qin)
 
         if z != 0:
-            self.radiusOfCurvature = self.z*(1+(self.zR/self.z)**2)
+            self.radiusOfCurvature = z*(1+(zR/z)**2)
 
         else:
             self.radiusOfCurvature = np.inf
 
-        return self.radiusOfCurvature
+        return self
 
 
 
@@ -224,20 +197,20 @@ class beamq:
         # -- beamq.overlap --
         # Find the overlap fraction of 2 beams (assumes axial symmetry).
 
-        self.q1 = beam1.q
-        self.q2 = beam2.q
+        q1 = beam1.q
+        q2 = beam2.q
 
-        self.w1 = beam1.get_waistZ(beam1)
-        self.w2 = beam2.get_waistZ(beam2)
+        w1 = beam1.get_waistZ(beam1)
+        w2 = beam2.get_waistZ(beam2)
 
         self.wavelength = beam1.wavelength
 
         if self.wavelength != beam2.wavelength:
             raise Exception ("Cannot overlap beams of different wavelength.")
 
-        else:
-            return (2*np.pi/self.wavelength*self.w1*self.w2*1/abs(self.q2.conjugate()-self.q1))**2
-            # square for 2D modematching
+        fraction = (2*np.pi/self.wavelength*w1*w2*1/abs(q2.conjugate()-q1))**2
+        # square for 2D modematching
+        return self
 
 
 
@@ -250,13 +223,12 @@ class beamq:
         # This transforms the oldbeam object and placed the new object into
         # newbeam, using the ABCD matrix M.
 
-        self.qin = beamin.q
-        self.M = M
+        qin = beamin.q
 
-        self.qout = beamin.transformValue (self.qin, self.M)
+        qout = beamin.transformValue(qin, M)
 
-        self.beamout = beamin.duplicate(beamin)
-        self.beamout.q = self.qout
+        beamout = beamin.duplicate(beamin)
+        beamout.q = qout
 
         return self 
 
@@ -268,14 +240,16 @@ class beamq:
         # -- beamq.plotBeamWidth --
         # Given an array of beamq objects, this function will plot 
         # the beamwidth.
-
-        self.zdomain = zdomain
         
-        self.ploth = plt.plot(self.zdomain, [q.get_beamWidth(q) for q in qarray], *args)
-        plt.plot(self.zdomain, [-q.get_beamWidth(q) for q in qarray], *args)
+        ploth = plt.plot(zdomain, [q.get_beamWidth(q) for q in qarray], *args)
+        plt.plot(zdomain, [-q.get_beamWidth(q) for q in qarray], *args)
 
         plt.show()
 
-        self.plothandle = self.ploth
+        plothandle = ploth
+
+        return self
+
+   
 
    
